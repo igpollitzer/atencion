08b1c108849f51dd57aa7c3934ab36364a9271f0
Iván G. Pollitzer <igpollitzer@gmail.com>
2016-02-14 06:52:15 -0300

    Agregué un menú con las opciones, terminé?

--- atencion.py
+++ atencion.py
@@ -38,6 +38,11 @@ class Parametros(object):
         self.pases = 0
         self.numero = 0
         self.cuanto = 0
+        self.mensaje_tiempo = 1
+        self.mensaje_transparencia = 25
+        self.pase_tiempo = 0.5
+        self.pase_minimo = 0.75
+        self.pase_maximo = 1
 
 globales = Parametros()
 
@@ -71,25 +76,23 @@ class Pelota(object):
     def pasar(self, jugador):
         self.posicion = self.get_pos(globales.cuanto)
         self.partida = globales.cuanto
-        # este arreglo de 35 esta horrible, pero sirve para 7 jugadores
-        arribo = globales.cuanto + 30
+        arribo = globales.cuanto + globales.pase_tiempo
         self.destino = jugador.get_pos(arribo)
-        self.paso = (self.destino - self.posicion) / 30.
+        self.paso = (self.destino - self.posicion) / float(globales.pase_tiempo)
         self.desfasaje = jugador.desfasaje
         self.estado = Pelota.VOLANDO
-        self.color = VERDE
-
+    
     def get_pos(self, tiempo):
     
         salida = np.array((0, 0))
     
-        if self.estado == Pelota.GIRANDO:
+        if self.estado is Pelota.GIRANDO:
     
             x = int(RADIO_GIRO * np.sin((tiempo + self.desfasaje) * 2 * np.pi / PERIODO) + CENTRO_GIRO[0])
             y = int(RADIO_GIRO * np.cos((tiempo + self.desfasaje) * 2 * np.pi / PERIODO) + CENTRO_GIRO[1])
             salida = np.array((x, y))
 
-        if self.estado == Pelota.VOLANDO:
+        if self.estado is Pelota.VOLANDO:
 
             self.posicion = self.posicion + self.paso
 
@@ -125,7 +128,7 @@ def programa():
     jugadores = get_jugadores(globales.nro_jugadores)
     pelota = Pelota()
     globales.cuanto = 0
-    arroja = randrange(10, 20, 1)
+    arroja = randrange(globales.pase_minimo, globales.pase_maximo, 1)
     globales.pases = 0
 
     globales.numero = str(randrange(101, 999, 1))
@@ -136,86 +139,66 @@ def programa():
     mensaje.set_alpha(0)
     mensaje_flag = 0
 
-    while True:# globales.cuanto < 601:
+    while globales.cuanto >= 0:
 
-            screen.fill(SCREEN_COLOR)
-            screen.blit(mensaje, (SCREEN_WIDTH/3, SCREEN_HEIGHT/3))
+        screen.fill(SCREEN_COLOR)
+        screen.blit(mensaje, (SCREEN_WIDTH/3, SCREEN_HEIGHT/3))
 
 
-            for event in pg.event.get():
-                if event.type == QUIT:
+        for event in pg.event.get():
+            if event.type is QUIT:
+                pg.quit()
+                return
+            if event.type is KEYDOWN:
+                if event.key is K_ESCAPE:
                     pg.quit()
                     return
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        pg.quit()
-                        return
-                    if event.key == K_UP:
-                        if pelota.estado == Pelota.GIRANDO:
-                            print "volando"
+                if event.key is K_UP:
+                    if pelota.estado is Pelota.GIRANDO:
+                        rand = randrange(0, globales.nro_jugadores, 1)
+                        while jugadores[rand].desfasaje is pelota.desfasaje:
                             rand = randrange(0, globales.nro_jugadores, 1)
-                            while jugadores[rand].desfasaje == pelota.desfasaje:
-                                rand = randrange(0, globales.nro_jugadores, 1)
-                            print rand
-                            print pelota.get_pos(globales.cuanto)
-                            arribo = 60
-                            print jugadores[rand].get_pos(arribo)
-                            pelota.pasar(jugadores[rand])
-                    if event.key == K_DOWN:
-                        pelota.__init__()
-                    if event.key == K_LEFT:
-                        pass
-                    if event.key == K_RIGHT:
-                        pass
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == LEFT_BUTTON:
-                        pass
-            # de cada fuente...
-            for jugador in jugadores:
-                pg.draw.circle(screen, jugador.color, jugador.get_pos(globales.cuanto), 40, 10)
+                        pelota.pasar(jugadores[rand])
+                if event.key is K_DOWN:
+                    pelota.__init__()
+                if event.key is K_LEFT:
+                    pass
+                if event.key is K_RIGHT:
+                    pass
+            if event.type is MOUSEBUTTONDOWN:
+                if event.button is LEFT_BUTTON:
+                    pass
+        # de cada fuente...
+        for jugador in jugadores:
+            pg.draw.circle(screen, jugador.color, jugador.get_pos(globales.cuanto), 40, 10)
 
-            pg.draw.circle(screen, pelota.color, pelota.get_pos(globales.cuanto), 30, 30)
+        pg.draw.circle(screen, pelota.color, pelota.get_pos(globales.cuanto), 30, 30)
 
-            if globales.cuanto == arroja:
+        if globales.cuanto >= arroja and pelota.estado is not pelota.VOLANDO:
+            rand = randrange(0, globales.nro_jugadores, 1)
+            while jugadores[rand].desfasaje is pelota.desfasaje:
                 rand = randrange(0, globales.nro_jugadores, 1)
-                while jugadores[rand].desfasaje == pelota.desfasaje:
-                    rand = randrange(0, globales.nro_jugadores, 1)
-                arribo = 60
-                pelota.pasar(jugadores[rand])
-                arroja = globales.cuanto + randrange(45, 60, 1)
-                if jugadores[rand].color == BLANCO:
-                    globales.pases = globales.pases + 1
-                    print globales.pases
+            pelota.pasar(jugadores[rand])
+            arroja = globales.cuanto + randrange(globales.pase_minimo, globales.pase_maximo, 1)
+            if jugadores[rand].color is BLANCO:
+                globales.pases = globales.pases + 1
 
-
-            if globales.cuanto > texto_fondo:
-                if mensaje_flag == 0:
-                    mensaje.set_alpha(mensaje.get_alpha() + 1)
-                elif mensaje_flag == 1:
-                    mensaje.set_alpha(mensaje.get_alpha() - 1)
-
-    #            print mensaje.get_alpha()
-
-                if globales.cuanto > texto_fondo + 30:
-                    mensaje_flag = 1
-    #                print "apago"
-    #            if globales.cuanto > texto_fondo + 90:
-    #                mensaje_flag = 2
-    #                print "listo" 
-    #            if globales.cuanto > texto_fondo + 121:
-    #                mensaje_flag = 3
-
-                if globales.cuanto > texto_fondo + 61:
+        if globales.cuanto > texto_fondo:
+            if mensaje_flag is 0 and mensaje.get_alpha() < globales.mensaje_transparencia:
+                mensaje.set_alpha(mensaje.get_alpha() + 1)
+            if mensaje_flag is 1:
+                mensaje.set_alpha(mensaje.get_alpha() - 1)
+                if mensaje.get_alpha() is 0:
                     mensaje_flag = 2
+            if globales.cuanto == texto_fondo + globales.mensaje_tiempo:
+                mensaje_flag = 1
+                
+        pg.display.update()
 
-            # muestro la pantalla
-
-            pg.display.update()
-
-            globales.cuanto = globales.cuanto + 1
-	    # print relos.get_fps()
-            relos.tick(FPS)
-
+        globales.cuanto = globales.cuanto + 1
+        relos.tick(FPS)
+        if globales.cuanto > 601 and pelota.estado is Pelota.GIRANDO:
+            pg.quit()
 
 
 with window("Atención"):
@@ -225,11 +208,37 @@ with window("Atención"):
         set_jugadores = editBox(globales.nro_jugadores)
         label(" jugadores.")
 
+    with flow(padx=10):
+        set_tiempo = editBox(globales.mensaje_tiempo)
+        label(" segundos.")
+
+    with flow(padx=10):
+        set_transparencia = editBox(globales.mensaje_transparencia)
+        label("% de transparencia.")
+
+    with flow(padx=10):
+        set_pase = editBox(globales.pase_tiempo)
+        label(" segundos de pase.")
+
+    with flow(padx=10):
+        set_minimo = editBox(globales.pase_minimo)
+        label(" segundos mínimo.")
+
+    with flow(padx=10):
+        set_maximo = editBox(globales.pase_maximo)
+        label(" segundos máximo.")
+
     @button("Cómo fue?")
     def boton_rta():
-        showInfo(message = (globales.pases, globales.numero))
+        showInfo(message = ("Se realizaron ", globales.pases, " pases, y el número fué el ", globales.numero, "."))
 
-    @button("Arrrrrrrrrrrrrrrrrrrranca!")
+    @button("Arranca!")
     def boton_arranca():
         globales.nro_jugadores=int(set_jugadores.text)
+        globales.mensaje_tiempo=int(set_tiempo.text) * FPS
+        globales.mensaje_transparencia=int(int(set_transparencia.text) * 2.55)
+        globales.pase_tiempo=int(float(set_pase.text) * FPS)
+        globales.pase_minimo=int(float(set_minimo.text) * FPS)
+        globales.pase_maximo=int(float(set_maximo.text) * FPS)
+		
         programa()
